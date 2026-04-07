b80418cc4f57baf3dd3b8384589ad1c1684cfd7d
ssirec <125913769+ssirec@users.noreply.github.com>
2026-04-07 19:08:54 +0200

    Refactor root component for head metadata management
    Refactored root component to use DocumentHead for metadata and removed head section from JSX.

--- src/root.tsx
+++ src/root.tsx
@@ -1,5 +1,10 @@
 import { component$, useStyles$ } from '@builder.io/qwik';
-import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
+import {
+  QwikCityProvider,
+  RouterOutlet,
+  ServiceWorkerRegister,
+  type DocumentHead,
+} from '@builder.io/qwik-city';
 import styles from './assets/styles/global.css?inline';
 
 export default component$(() => {
@@ -7,87 +12,36 @@ export default component$(() => {
 
   return (
     <QwikCityProvider>
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Servis telefonov Nova Gorica | Popravilo mobitelov</title>
-        <meta
-          name="description"
-          content="Servis telefonov Nova Gorica. Popravilo zaslona, baterije, polnjenja. Hitra in zanesljiva storitev brez naročanja."
-        />
-        <meta
-          name="keywords"
-          content="servis telefonov Nova Gorica, popravilo mobitelov, menjava zaslona, baterije, servis iPhone Nova Gorica"
-        />
-      </head>
+      <RouterOutlet />
+      <ServiceWorkerRegister />
 
-      <body lang="sl">
-        <RouterOutlet />
-        <ServiceWorkerRegister />
-
-        {/* Tawk.to Chat */}
-        <div id="tawk_56c73659b74a0813627d275c"></div>
-        <script
-          dangerouslySetInnerHTML={`
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            Tawk_API.embedded='tawk_56c73659b74a0813627d275c';
-            (function(){
-              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-              s1.async=true;
-              s1.src='https://embed.tawk.to/56c73659b74a0813627d275c/1gsshkkuq';
-              s1.charset='UTF-8';
-              s1.setAttribute('crossorigin','*');
-              s0.parentNode.insertBefore(s1,s0);
-            })();
-          `}
-        />
-      </body>
-    </QwikCityProvider>
-  );
-});import { component$, useStyles$ } from '@builder.io/qwik';
-import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
-import styles from './assets/styles/global.css?inline';
-
-export default component$(() => {
-  useStyles$(styles);
-
-  return (
-    <QwikCityProvider>
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Servis telefonov Nova Gorica</title>
-        <meta
-          name="description"
-          content="Servis telefonov Nova Gorica. Popravilo zaslona, baterije, polnjenja. Hitra in zanesljiva storitev brez naročanja."
-        />
-        <meta
-          name="keywords"
-          content="servis telefonov Nova Gorica, popravilo mobitelov, menjava zaslona, baterije, servis iPhone Nova Gorica"
-        />
-      </head>
-
-      <body lang="sl">
-        <RouterOutlet />
-        <ServiceWorkerRegister />
-
-        {/* Tawk.to Chat */}
-        <div id="tawk_56c73659b74a0813627d275c"></div>
-        <script
-          dangerouslySetInnerHTML={`
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            Tawk_API.embedded='tawk_56c73659b74a0813627d275c';
-            (function(){
-              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-              s1.async=true;
-              s1.src='https://embed.tawk.to/56c73659b74a0813627d275c/1gsshkkuq';
-              s1.charset='UTF-8';
-              s1.setAttribute('crossorigin','*');
-              s0.parentNode.insertBefore(s1,s0);
-            })();
-          `}
-        />
-      </body>
+      {/* Tawk.to Chat */}
+      <div id="tawk_56c73659b74a0813627d275c" />
+      <script
+        dangerouslySetInnerHTML={`
+          var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+          Tawk_API.embedded='tawk_56c73659b74a0813627d275c';
+          (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/56c73659b74a0813627d275c/1gsshkkuq';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+          })();
+        `}
+      />
     </QwikCityProvider>
   );
 });
+
+// Define head metadata using QwikCity DocumentHead
+export const head: DocumentHead = {
+  title: 'Servis telefonov Nova Gorica | Popravilo mobitelov',
+  meta: [
+    { name: 'description', content: 'Servis telefonov Nova Gorica. Popravilo zaslona, baterije, polnjenja. Hitra in zanesljiva storitev brez naročanja.' },
+    { name: 'keywords', content: 'servis telefonov Nova Gorica, popravilo mobitelov, menjava zaslona, baterije, servis iPhone Nova Gorica' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { charSet: 'utf-8' }
+  ]
+};
