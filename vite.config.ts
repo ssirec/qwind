0c979c03fb89246a53210bc467042286ad14e555
ssirec <125913769+ssirec@users.noreply.github.com>
2026-04-09 10:31:38 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,18 @@
 import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-
-import { SITE } from './src/config.mjs';
+import cloudflarePagesAdapter from '@builder.io/qwik-city/adapters/cloudflare-pages/vite';
 
 export default defineConfig(() => {
-    return {
-        base: SITE.basePathname,
-        plugins: [
-            qwikCity({
-                trailingSlash: SITE.trailingSlash,
-            }),
-            qwikVite(),
-            tsconfigPaths(),
-        ],
-        preview: {
-            headers: {
-                'Cache-Control': 'public, max-age=600',
-            },
-        }
-    };
+  return {
+    plugins: [
+      qwikCity(),
+      qwikVite(),
+      cloudflarePagesAdapter(),
+    ],
+    build: {
+      target: 'es2020',
+      minify: 'esbuild',
+    },
+  };
 });
