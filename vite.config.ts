788f9270e011f2f53bee77d0c60909eed03ff975
ssirec <125913769+ssirec@users.noreply.github.com>
2026-04-09 16:07:27 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,16 @@
 import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-
-import { SITE } from './src/config.mjs';
+import staticAdapter from '@builder.io/qwik-city/adapters/static/vite';
 
 export default defineConfig(() => {
-    return {
-        base: SITE.basePathname,
-        plugins: [
-            qwikCity({
-                trailingSlash: SITE.trailingSlash,
-            }),
-            qwikVite(),
-            tsconfigPaths(),
-        ],
-        preview: {
-            headers: {
-                'Cache-Control': 'public, max-age=600',
-            },
-        }
-    };
+  return {
+    plugins: [
+      qwikCity(),
+      qwikVite(),
+      staticAdapter({
+        origin: 'https://example.com'
+      }),
+    ],
+  };
 });
