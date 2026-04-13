f7d75a03985e60a47f7c78439a0ecda799094ff0
ssirec <125913769+ssirec@users.noreply.github.com>
2026-04-13 18:52:50 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
-import cloudflarePagesAdapter from '@builder.io/qwik-city/adapters/cloudflare-pages/vite';
+import { cloudflarePagesAdapter } from '@builder.io/qwik-city/adapters/cloudflare-pages/vite';
 
 export default defineConfig(() => {
   return {
