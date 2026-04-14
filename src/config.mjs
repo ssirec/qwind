da897e39ffe7c95b62e6b1ea74e5cb8316e389aa
ssirec <125913769+ssirec@users.noreply.github.com>
2026-04-14 12:19:41 +0200

    Update config.mjs

--- src/config.mjs
+++ src/config.mjs
@@ -1,12 +1,9 @@
+// src/config.ts
 export const SITE = {
   name: "Mediaservis",
-
-  title:
-    "Mediaservis popravilo mobilnih telefonov iPhone servis tablic",
-  description:
-    "Ulica tolminskih puntarjev 4 (1. nadstropje)",
-
+  title: "Mediaservis popravilo mobilnih telefonov iPhone servis tablic",
+  description: "Ulica tolminskih puntarjev 4 (1. nadstropje)",
   origin: "https://qwind.pages.dev",
   basePathname: "/",
   trailingSlash: true,
-};
+} as const;
